0aeb8e882a45daefc7a50ca6adf9c1cd19125fcb
MamunHossen1998 <mamunhossen149191@gmail.com>
2024-01-24 00:30:12 +0600

    operator

---
+++ js.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h4>Bangladesh is a small country</h4>
+    <button type="button" onclick="document.write(7+8)">Click</button>
+    <script>
+        document.write('I love my country');
+        window.alert(`Bangladesh`)
+        console.log(`Hi`);
+        let x = 10;
+        var y = 5;
+        if (y == 5) {
+            let x = 4;
+        }
+        console.log(Math.pow(x, y));
+    </script>
+</body>
+
+</html>

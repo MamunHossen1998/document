bf1deb1293a6601b693ee510054ca97e06773586
MamunHossen1998 <mamunhossen149191@gmail.com>
2024-01-27 22:28:38 +0600

    parEnd

--- js.php
+++ js.php
@@ -11,10 +11,14 @@
     <h4>Bangladesh is a small country</h4>
     <button type="button" onclick="document.write(7+8)">Click</button>
     <script>
-        document.write('I love my country');
-        console.log('mamun \"is\"a student');
-        var len = 'Mamun ';
-        console.log(`length ${len.charAt(0)}`);
+        let text1 = "Mamun";
+        let text2 = " Mamun  ";
+        let text3 = text2.trimEnd();
+        console.log(`text1 length ${text1.length}`);
+        console.log(`text2 length ${text2.length}`);
+        console.log(`text3 length ${text3.length}`);
+        let n1 = 'Mamun ';
+        console.log(`after pads ${n1.padEnd(10,0)}`);
     </script>
 </body>
 

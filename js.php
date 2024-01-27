f3b600bd5d96ae2add2b38da0caaa84038944f08
MamunHossen1998 <mamunhossen149191@gmail.com>
2024-01-28 00:03:59 +0600

    replace

--- js.php
+++ js.php
@@ -11,14 +11,8 @@
     <h4>Bangladesh is a small country</h4>
     <button type="button" onclick="document.write(7+8)">Click</button>
     <script>
-        let text1 = "Mamun";
-        let text2 = " Mamun  ";
-        let text3 = text2.trimEnd();
-        console.log(`text1 length ${text1.length}`);
-        console.log(`text2 length ${text2.length}`);
-        console.log(`text3 length ${text3.length}`);
-        let n1 = 'Mamun ';
-        console.log(`after pads ${n1.padEnd(10,0)}`);
+        let text1 = "This is table. This is a chair";
+        console.log(`${text1.replaceAll('This','I')}`);
     </script>
 </body>
 
